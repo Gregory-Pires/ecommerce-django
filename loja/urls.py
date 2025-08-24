12d9fa93ef14fc2828e3b9ce052d8267d5a055f5
Greogry-Pires <blene.pires@gmail.com>
2025-08-24 14:42:41 -0300

    Produtos por categoria

--- loja/urls.py
+++ loja/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.loja, name='loja'),
+    path('<slug:slug_categoria>/', views.loja, name='produtos_por_categoria'),
 ]
--- loja/views.py
+++ loja/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Produto
+from categoria.models import Categoria
 
 # Create your views here.
 
-def loja(request):
-    produtos = Produto.objects.all().filter(esta_disponível=True)
-    contador_produtos = produtos.count()
+def loja(request, slug_categoria=None):
+    categorias = None
+    produtos = None
+
+    if slug_categoria !=None:
+        categorias = get_object_or_404(Categoria, slug=slug_categoria)
+        produtos = Produto.objects.filter(categoria=categorias, esta_disponível=True)
+        contador_produtos = produtos.count()
+    else:
+        produtos = Produto.objects.all().filter(esta_disponível=True)
+        contador_produtos = produtos.count()
 
     context = {
         'produtos': produtos,
