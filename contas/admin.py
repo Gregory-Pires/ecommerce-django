7e5cd3d52eaf6c51cd71c77f8a59311a3a041758
Greogry-Pires <blene.pires@gmail.com>
2025-08-24 00:48:40 -0300

    Modificação adm

--- contas/admin.py
+++ contas/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Conta
 
 # Register your models here.
-admin.site.register(Conta)
+class ContaAdm(UserAdmin):
+    list_display = ('email', 'nome', 'sobrenome', 'nome_usuário', 'ultimo_login', 'data_cadastro', 'is_active')
+    ordering = ('email',)
+    list_display_links = ('email', 'nome', 'sobrenome')
+    readonly_fields = ('ultimo_login', 'data_cadastro')
+    ordering = ('-data_cadastro',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Conta, ContaAdm)
