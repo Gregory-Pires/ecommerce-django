584fd08547ef0e8553c5cc625784c525d984f7d5
Greogry-Pires <blene.pires@gmail.com>
2025-08-24 14:22:25 -0300

    Mostar produtos na pagina da loja

--- loja/views.py
+++ loja/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
 
 def loja(request):
-    return render(request, 'loja/loja.html')
+    produtos = Produto.objects.all().filter(esta_disponível=True)
+    contador_produtos = produtos.count()
+
+    context = {
+        'produtos': produtos,
+        'contador_produtos': contador_produtos,
+    }
+    return render(request, 'loja/loja.html', context)
