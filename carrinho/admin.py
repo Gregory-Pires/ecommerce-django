5f2ee6e989715504a652daf6a999a857d9c3e928
Greogry-Pires <blene.pires@gmail.com>
2025-08-24 20:06:38 -0300

    Carrinho e CarrinhoItem model

--- carrinho/admin.py
+++ carrinho/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Carrinho, CarrinhoItem
+
+admin.site.register(Carrinho)
+admin.site.register(CarrinhoItem)
 
 # Register your models here.
--- carrinho/models.py
+++ carrinho/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from loja.models import Produto
 
 # Create your models here.
+
+class Carrinho(models.Model):
+    carrinho_id = models.CharField(max_length=250, blank=True)
+    data_criação = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.carrinho_id
+    
+class CarrinhoItem(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    carrinho = models.ForeignKey(Carrinho, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+    esta_ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.produto
