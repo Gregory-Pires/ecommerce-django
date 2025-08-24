7d745820ea05f7e67a88dd330a188670390357cd
Greogry-Pires <blene.pires@gmail.com>
2025-08-24 19:23:07 -0300

    Carrinho app e template

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('loja/', include('loja.urls')),
+    path('carrinho/', include('carrinho.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
