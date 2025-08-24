a655cfe020b647131a0b854434535acdcd3b44c4
Greogry-Pires <blene.pires@gmail.com>
2025-08-24 01:36:57 -0300

    Prepopulando o slug da categoria

--- categoria/admin.py
+++ categoria/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Categoria
 
 # Register your models here.
-admin.site.register(Categoria)
+class CategoriaAdm(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('nome_categoria',)}
+    list_display = ('nome_categoria', 'slug')
+
+admin.site.register(Categoria, CategoriaAdm)
--- categoria/models.py
+++ categoria/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Categoria(models.Model):
-    nome_categria = models.CharField(max_length=50, unique=True)
-    slug = models.CharField(max_length=100, unique=True)
+    nome_categoria = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
     descrição = models.TextField(max_length=255, blank=True)
     imagem_cat = models.ImageField(upload_to='photos/categorias', blank=True)
 
     def __str__(self):
-        return self.nome_categria
+        return self.nome_categoria
