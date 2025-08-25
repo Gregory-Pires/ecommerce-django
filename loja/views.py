9a36f7fddaacd08b15f2578ab449bbcd817719b2
Greogry-Pires <blene.pires@gmail.com>
2025-08-25 20:29:14 -0300

    Django paginator

--- loja/views.py
+++ loja/views.py
@@ -4,6 +4,7 @@ from categoria.models import Categoria
 from carrinho.models import CarrinhoItem
 
 from carrinho.views import _carrinho_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 
 def loja(request, slug_categoria=None):
@@ -13,13 +14,19 @@ def loja(request, slug_categoria=None):
     if slug_categoria !=None:
         categorias = get_object_or_404(Categoria, slug=slug_categoria)
         produtos = Produto.objects.filter(categoria=categorias, esta_disponível=True)
+        paginator = Paginator(produtos, 3)
+        page = request.GET.get('page')
+        paged_produtos = paginator.get_page(page)
         contador_produtos = produtos.count()
     else:
         produtos = Produto.objects.all().filter(esta_disponível=True)
+        paginator = Paginator(produtos, 3)
+        page = request.GET.get('page')
+        paged_produtos = paginator.get_page(page)
         contador_produtos = produtos.count()
 
     context = {
-        'produtos': produtos,
+        'produtos': paged_produtos,
         'contador_produtos': contador_produtos,
     }
     return render(request, 'loja/loja.html', context)
